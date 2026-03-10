d3639ab9677797992c3280b883e2edcb1c696901
高晓睿 <2639700355@qq.com>
2026-03-10 18:53:19 +0800

    Exercise #4

--- MyProject/src/ERyder.java
+++ MyProject/src/ERyder.java
@@ -1,65 +1,55 @@
 public class ERyder {
+    private static final String COMPANY_NAME = "ERyder";
+    private static final double BASE_FARE = 1.0;
+    private static final double PER_MINUTE_FARE = 0.5;
+
+    private final String LINKED_ACCOUNT;
+    private final String LINKED_PHONE_NUMBER;
+
+    private int totalUsageInMinutes;
+    private double totalFare;
     private String bikeID;
     private int batteryLevel;
     public boolean isAvailable;
-    public float kmDriven;
-    
+    public double kmDriven;
 
-public ERyder(String bikeID, int batteryLevel, boolean isAvailable, float kmDriven){
-    this.bikeID = bikeID;
-    this.batteryLevel = batteryLevel;
-    this.isAvailable = isAvailable;
-    this.kmDriven = kmDriven;
-}
-public ERyder(){
-    this.bikeID = "Unknown";
-    this.batteryLevel = 0;
-    this.isAvailable = false;
-    this.kmDriven = 0.0f;
-}
-public void ride(){
-    if(this.batteryLevel > 0 && this.isAvailable){
-        System.out.println("The bike isavailable.");
-    }else{
-        System.out.println("The bike is not available.");
-    }
-}
-public void printBikeDetails(){
-    System.out.println("Bike ID: " + this.bikeID);
-    System.out.println("Battery Level: " + this.batteryLevel);
-    System.out.println("Available: " + (this.isAvailable ? "Avaliable" : "Not Avaialbe"));
-    System.out.println("Distance Travalied: " + this.kmDriven + " km");
-}
-public String getBikeId(){
-    return bikeID;
-}
-public int getBatteryLevel(){
-    return batteryLevel;
-}
-public boolean getIsAvailable(){
-    return isAvailable;
-}
-public float getKmDriven(){
-    return kmDriven;
-}
-public void setBatteryLevel(int batteryLevel){
-    if (batteryLevel >= 0 && batteryLevel <= 100){
+    public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven, String linkedAccount, String linkedPhoneNumber) {
+        this.bikeID = bikeID;
         this.batteryLevel = batteryLevel;
-    }else{
-        System.out.println("Error: Battery level must be between 0 and 100.");
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+        this.LINKED_ACCOUNT = linkedAccount;
+        this.LINKED_PHONE_NUMBER = linkedPhoneNumber;
+
     }
-}
-public void setIsAvailable(boolean isAvailable){
-    this.isAvailable = isAvailable;
-}
-public void setKmDriven(float kmDriven){
-    this.kmDriven = kmDriven;
-}
-public static void main(String[]args){
-    ERyder bike = new ERyder("123456", 80, true, 15.5f);
-    bike.ride();
-    bike.printBikeDetails();
-    bike.setBatteryLevel(30);
-    System.out.println("Current battery: " + bike.getBatteryLevel());
-}
+    public ERyder(String bikeID, String linkedAccount, String linkedPhoneNumber){
+        this.bikeID = bikeID;
+        this.batteryLevel = 100;
+        this.isAvailable = true;
+        this.kmDriven = 0.0;
+        this.LINKED_ACCOUNT = linkedAccount;
+        this.LINKED_PHONE_NUMBER = linkedPhoneNumber;
+    }
+    public void printRideDetails(int totalUsageInMinutes){
+        this.totalUsageInMinutes = totalUsageInMinutes;
+        this.totalFare = calculateFare(totalUsageInMinutes);
+       
+        System.out.println("BikeID" + this.bikeID);
+        System.out.println("Linked Account" + LINKED_ACCOUNT);
+        System.out.println("Linked phone number" + LINKED_PHONE_NUMBER);
+        System.out.println("Usage time" + totalUsageInMinutes + " minutes");
+        System.out.println("Total fare: " + totalFare);
+    }
+    private double calculateFare(int usageInMinutes){
+        return BASE_FARE + (PER_MINUTE_FARE * usageInMinutes);
+      
+    }
+    public static void main(String[] args){
+        ERyder bike1 = new ERyder("12345", 80, true, 15.5, "HD2345", "123456");
+        ERyder bike2 = new ERyder("123456", "JG3456", "98765");
+        bike1.printRideDetails(30);
+        System.out.println();
+        bike2.printRideDetails(40);
+    }
+    
 }
